5820cab0d4946da4ef30dab46db32cb53bcb2917
Kethytektek <163605882+Kethytektek@users.noreply.github.com>
2024-04-05 15:47:08 -0300

    soletre

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite o texto a ser soletrado:");
+
+Console.WriteLine();
+
+string TextoSoletrado = Console.ReadLine()!;
+
+Console.WriteLine("\nSoletrado fica:\n");
+
+Console.WriteLine(string.Join("-",TextoSoletrado.ToUpper().ToCharArray()));
+
+Console.WriteLine();
